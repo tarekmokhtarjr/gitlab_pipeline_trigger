910541e1fdbd2d8332f7c7aa10456cfed7ecdcbc
atarekmokhtar90 <atarekmokhtar90@gmail.com>
2023-05-01 00:11:11 +0200

    [UPD] Rename thumbnail

--- gitlab_pipeline_trigger/__manifest__.py
+++ gitlab_pipeline_trigger/__manifest__.py
@@ -11,7 +11,7 @@
     "version": "15.0.1",
     "license": "GPL-3",
     "depends": ["project"],
-    "images": "static/description/thmubnail.png",
+    "images": ["static/description/logo.png"],
     "data": [
         "security/ir.model.access.csv",
         "views/gitlab_jobs_views.xml",
